0bb5976a047563e381a1cd1c27dece5f16aebcd2
Kevin Huang <yellow.khaos@gmail.com>
2018-10-09 04:27:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,58 @@
+let driverId = 0
+let tripId = 0
+let passengerId = 0
+let store = {drivers: [], passengers: [], trips:[]}
+
+class Driver {
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function(item){return this.id === item.driverId}.bind(this))
+  }
+
+  passengers() {
+    let tripList = this.trips()
+    let passengers = tripList.map(function(trip){return store.passengers.find(function(passId){return passId.id === trip.passengerId})})
+    return passengers
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(function(item){return this.id === item.passengerId}.bind(this))
+  }
+
+  drivers() {
+    let tripList = this.trips()
+    let drivers = tripList.map(function(trip){return store.drivers.find(function(driverId){return driverId.id===trip.driverId})})
+    return drivers
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = ++tripId
+    store.trips.push(this)
+  }
+
+  passenger() {
+    return store.passengers.find(function(item){return item.id === this.passengerId}.bind(this))
+  }
+
+  driver() {
+    return store.drivers.find(function(item){return item.id===this.driverId}.bind(this))
+  }
+
+}
